392e41141152b592a934759ed6d547b9a3827283
Lis1337 <il4991@yandex.ru>
2020-06-29 23:07:06 +0500

    0 review

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from .views import PostViewSet, CommentViewSet, FollowList, GroupList
+
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet)
 
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('follow/', FollowList.as_view()),
+    path('group/', GroupList.as_view())
 ]
--- api/views.py
+++ api/views.py
@@ -1,1 +1,67 @@
 # TODO:  Напишите свой вариант
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, permissions, status, filters, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.exceptions import ValidationError
+from django.db import models
+
+from api.models import User, Post, Comment, Follow, Group
+from .serializers import PostSerializer, CommentSerializer, FollowSerializer, GroupSerializer
+from .permissions import IsOwnerOrReadOnly
+
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsOwnerOrReadOnly]
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ['group',]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [
+        IsOwnerOrReadOnly
+    ]
+
+    def get_queryset(self):
+        return Comment.objects.filter(post=self.kwargs['post_id'])
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class FollowList(generics.ListCreateAPIView):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['=user__username', '=following__username']
+
+    def perform_create(self, serializer):
+        try:
+            following = User.objects.get(username=self.request.data.get('following'))
+            follow = Follow.objects.filter(following=following, user=self.request.user).exists()
+        except User.DoesNotExist:
+            raise ValidationError('user doesnt exist')
+
+        if self.request.user == following:
+            raise ValidationError('Cant subscribe to yourself')
+        if follow:
+            raise ValidationError('You have already signed up')
+        serializer.save(user=self.request.user, following=following)
+        
+
+class GroupList(generics.ListCreateAPIView):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    filterset_fields = ['=group__title']
